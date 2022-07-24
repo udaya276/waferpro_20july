916ba88cf1097f9df58e6c3ff2cc5a5d63768d80
udaya276 <udaya276@gmail.com>
2022-07-24 18:08:48 +0530

    main.py changed

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    return "Flask app is running on 20th july"
 
 port = int(os.getenv("PORT", 5001))
 
